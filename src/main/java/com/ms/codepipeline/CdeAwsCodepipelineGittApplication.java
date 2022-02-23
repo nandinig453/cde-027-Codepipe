3a735cb7812a51ef7be78741ebcb2ce5c92f19c6
nandini <nandinig453@gmail.com>
2022-02-23 17:33:16 +0530

    initial commit

--- src/main/java/com/ms/codepipeline/CdeAwsCodepipelineGittApplication.java
+++ src/main/java/com/ms/codepipeline/CdeAwsCodepipelineGittApplication.java
@@ -2,9 +2,23 @@ package com.ms.codepipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
+
 public class CdeAwsCodepipelineGittApplication {
+	@GetMapping("/")
+	public String hello()
+	{
+		return "Helloworld";
+	}
+	@GetMapping("/welcome")
+	public String welcome()
+	{
+		return " Code pipeline";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CdeAwsCodepipelineGittApplication.class, args);
