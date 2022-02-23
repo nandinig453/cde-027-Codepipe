7790c043e3cdb958fe99256764e5998de75fa01d
nandini <nandinig453@gmail.com>
2022-02-23 10:16:34 +0530

    initial commit

---
+++ src/main/java/com/ms/codepipeline/CdeAwsCodepipelineGittApplication.java
@@ -0,0 +1,13 @@
+package com.ms.codepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdeAwsCodepipelineGittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdeAwsCodepipelineGittApplication.class, args);
+	}
+
+}
